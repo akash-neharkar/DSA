4755289fab39289313a33516944dece10da0fab7
Akash Neharkar <124196448+akash-neharkar@users.noreply.github.com>
2024-08-04 18:32:17 +0000

    str

---
+++ string_125_easy.java
@@ -0,0 +1,18 @@
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        clean = ''.join(filter(str.isalnum, str(s))).lower()
+        return clean == clean[::-1]
